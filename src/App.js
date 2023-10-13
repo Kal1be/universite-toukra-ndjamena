29f059e147c89a990257e172e9dc55d9b2493613
kal1be <yahoulbofelix@gmail.com>
2023-10-13 21:08:41 +0100

    modification

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import Navbar from './header/Navbar';
 import "bootstrap/dist/css/bootstrap.min.css"
-import {Routes,Route } from 'react-router-dom';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import Accueil from './pages/Accueil';
 import Footer from './footer/Footer';
 
 function App() {
   return (
-    <div>
+    <div> 
+      <Router>
       <Navbar/>
-       <Routes>
-        <Route path='/' Component={<Accueil/>}/>
-      </Routes>
+      <Switch>
+      <Route exact path='/' Component={Accueil}/>
+      </Switch>
       <Footer/>
+      </Router>
     </div>
   );
 }
