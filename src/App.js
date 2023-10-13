1629bb8294fa5c77a4edbcda571e93ec93c7e49e
kal1be <yahoulbofelix@gmail.com>
2023-10-13 22:03:56 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import Navbar from './header/Navbar';
 import "bootstrap/dist/css/bootstrap.min.css"
-import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+import {Route,Routes} from 'react-router-dom';
 import Accueil from './pages/Accueil';
 import Footer from './footer/Footer';
 
 function App() {
   return (
     <div> 
-      <Router>
       <Navbar/>
-     <Accueil/>
+    <Routes>
+      <Route path='/' element={<Accueil/>}/>
+     </Routes>
       <Footer/>
-      </Router>
     </div>
   );
 }
